d873c272fc079f928efd10fbb55698d7c17289ba
Victoriia-Ripka <vikaripka@gmail.com>
2022-05-17 18:59:48 +0200

    Vika)

---
+++ Vika.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+//ім'я персонажа, іменник у множині, число, частина тіла, дієслово
+
+int is_numeric(char numeric[99])
+{
+    for (int i = 0; numeric[i] != '\0'; i++)
+        if (numeric[i] < 48 || numeric[i] > 57)
+        {
+            cout << "Not correct. Input numeric" << endl;
+            cin.getline(numeric, 99);
+            is_numeric(numeric);
+        }
+    return 0;
+}
+
+int is_part_of_the_body(char body[99])
+{
+    ifstream f;
+    f.open("parts_of_the_body.txt");
+    if (!f.is_open())
+    {
+        cout << "error opening file" << endl;
+        return 0;
+    }
+
+    char p_o_t_b[9];
+    do
+    {
+        f >> p_o_t_b;
+    } while (strcmp(p_o_t_b, body) != 0 && !f.eof() && (f.get() != '\0'));
+
+    if (strcmp(p_o_t_b, body) != 0)
+    {
+        cout << "Not correct. Input part of the body" << endl;
+        cin.getline(body, 99);
+        is_part_of_the_body(body);
+    }
+    f.close();
+    return 0;
+}
+
+int is_verb(char verb[99])
+{
+    ifstream f;
+    f.open("dictionary_of_verbs.txt");
+    if (!f.is_open())
+    {
+        cout << "error opening file" << endl;
+        return 0;
+    }
+
+    char d_o_v[10];
+    do
+    {
+        f >> d_o_v;
+    } while (strcmp(d_o_v, verb) != 0 && !f.eof() && (f.get() != '\0'));
+
+    if (strcmp(d_o_v, verb) != 0)
+    {
+        cout << "Not correct. Input verb" << endl;
+        cin.getline(verb, 99);
+        is_verb(verb);
+    }
+    f.close();
+    return 0;
+}
+
+int main()
+{
+    char name[99],
+        noun[99],
+        numeric[99],
+        body[99],
+        verb[99];
+
+    cout << "Input name" << endl;
+    cin.getline(name, 99);
+    while (!isupper(name[0]) || name[1] == '\0' || name[0] == '\0')
+    {
+        cout << "Not correct. Input name" << endl;
+        cin.getline(name, 99);
+    }
+
+    cout << "Input noun in the plural" << endl;
+    cin.getline(noun, 99);
+    if (noun[2] == '\0' || noun[0] == '\0' || noun[1] == '\0' || noun[sizeof(noun) - 1] != 's')
+    {
+        cout << "Not correct. Input noun in the plural" << endl;
+        cin.getline(noun, 99);
+    }
+
+    cout << "Input numeric (used numbers 0-9)" << endl;
+    cin.getline(numeric, 99);
+    is_numeric(numeric);
+
+    cout << "Input part of the body" << endl;
+    cin.getline(body, 99);
+    is_part_of_the_body(body);
+
+    cout << "Input verb" << endl;
+    cin.getline(verb, 99);
+    is_verb(verb);
+
+    cout << name << " " << noun << " " << numeric << " " << body << " " << verb << endl;
+    return 0;
+}
--- a.cpp
+++ a.cpp
@@ -1,6 +1,5 @@
 //учимось працювати з гітхабом(не виходить)
 
-
 #include <iostream>
 #include <map>
 #include <vector>
@@ -12,16 +11,19 @@ using namespace std;
 typedef deque<string> Prefix;
 map<Prefix, vector<string>> statetab;
 
-enum{
-	NPREF = 2,     //number of prefixes  //1 for total fun 
-	NHASH = 10000,  //4093, /*size of state hash table array*/
-	MAXGEN = 10000 //maximum words generated
+enum
+{
+	NPREF = 2,	   // number of prefixes  //1 for total fun
+	NHASH = 10000, // 4093, /*size of state hash table array*/
+	MAXGEN = 10000 // maximum words generated
 };
 char NONWORD[] = "/n"; /*cannot appear as a word*/
 
 // add: add word to suffix list, update prefix
-void add(Prefix& prefix, const string& s){
-	if (prefix.size() == NPREF){
+void add(Prefix &prefix, const string &s)
+{
+	if (prefix.size() == NPREF)
+	{
 		statetab[prefix].push_back(s);
 		prefix.pop_front();
 	}
@@ -29,21 +31,24 @@ void add(Prefix& prefix, const string& s){
 }
 
 // build: read input words, build state table
-void build(Prefix& prefix, istream& in) {
+void build(Prefix &prefix, istream &in)
+{
 	string buf;
 	while (in >> buf)
 		add(prefix, buf);
 }
 
-//generate: produce output, one word
-void generate(int nwords) {
+// generate: produce output, one word
+void generate(int nwords)
+{
 	Prefix prefix;
 	int i;
-	for (i=0; i<NPREF; i++) //reset initial prefix
+	for (i = 0; i < NPREF; i++) // reset initial prefix
 		add(prefix, NONWORD);
-	for (i=0; i<nwords; i++){
-		vector<string>& suf = statetab[prefix];
-		const string& w = suf[rand() % suf.size()];
+	for (i = 0; i < nwords; i++)
+	{
+		vector<string> &suf = statetab[prefix];
+		const string &w = suf[rand() % suf.size()];
 		if (w == NONWORD)
 			break;
 		cout << w << " ";
@@ -52,17 +57,16 @@ void generate(int nwords) {
 	}
 }
 
-
-
 ///   21:51 кам кам кам
-int main() {
-	//set random seed to try to generate different texts every time
+int main()
+{
+	// set random seed to try to generate different texts every time
 	srand(time(0));
 	cout << "Тяжко";
 	//Кам від Віталіка <3
-	int nwords = MAXGEN; 
-	Prefix prefix; // current input prefix
-	for (int i=0; i<NPREF; i++) //set up initial prefix
+	int nwords = MAXGEN;
+	Prefix prefix;					// current input prefix
+	for (int i = 0; i < NPREF; i++) // set up initial prefix
 		add(prefix, NONWORD);
 	build(prefix, cin);
 	add(prefix, NONWORD);
