914764165bbdc34093b545ec149a601c9fc27f5c
Vitalii <solonychnui.vitaliy@lll.kpi.ua>
2022-05-18 19:57:09 +0200

    1

--- script.h
+++ script.h
@@ -3,17 +3,17 @@
 //Туто клас сцена - в якому зберігається вся інформація про сцену
 class scene{
     public:
-    void set(int ls, int p, char n[SIZE], char text[SIZE*5]);
+    void set(int ls, char n[SIZE], char text[SIZE*5]);
     //Якщо 0 - то звичанйна сцена. 
     //Якщо 1-5 то пейзаш з бібліотеки
     int landscape; 
-    //0 - Шрек, 1 - Шрек + Пітун і так далі
-    int personen;
     //Ім'я говорячого в сцені
     //Якщо "\0" то слова автора
     char name[SIZE];
     //Відповідно тест сцени
     char text[SIZE*5];
 };
+
+
 
 
