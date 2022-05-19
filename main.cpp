ebca7cbc749bd8fe6af26f579e4b6caf3b2f1740
Vitalii <solonychnui.vitaliy@lll.kpi.ua>
2022-05-19 22:08:19 +0200

    1

--- main.cpp
+++ main.cpp
@@ -10,18 +10,15 @@
 int main()
 {
     scene script[N];
-    for(int i=0;i<N;i++){
-        script[i].set(i+1, "\0", pitun_speak(50));
-    }
     input info;     // структура для збереження даних, що вводить користувач 
-    introduction(); // функція, що ознайомлює користувача з ідеєю гри
+    //introduction(); // функція, що ознайомлює користувача з ідеєю гри
     f_input(&info); // функція вводу даних користоувачем
-    // cut_paste(info);
-    cout << info.name << " " << info.noun << " " << info.numeric << " " << info.body << " " << info.verb << endl;
+    cut_paste(info, &script[0]);
+    cout <<"Name: "<< info.name << " " << info.noun << " " << info.numeric << " " << info.body << " " << info.verb << endl;
     
-    for(int i=0; i<N; i++){ //ітераційний цикл що виводить в консоль сторінки історії
-    show_scene (script[i]); //функція виводу ілюстрації, роздільної лінії та репліки в консоль
-    getchar();              //очікування на "команду продовження"
-    }
+    // for(int i=0; i<3; i++){ //ітераційний цикл що виводить в консоль сторінки історії
+    // show_scene (script[i]); //функція виводу ілюстрації, роздільної лінії та репліки в консоль
+    // getchar();              //очікування на "команду продовження"
+    // }
     return 0;
 }
