f25409cf6e99e777c57f2d565c80a54e56c24522
syniukdima <syniukdima@gmail.com>
2022-05-19 13:49:02 +0300

    генератор пітуна з коментами

--- function.h
+++ function.h
@@ -1,6 +1,15 @@
 
 //Функції кидати сюди
 #include "script.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream> // opening file in pitun_speak()
+#include <map>  //statetab map in pitun_speak()
+#include <vector> //suffix in pitun_speak()
+#include <deque> //prefix in pitun_speak()
+
+using namespace std;
 
 class input{
     public:
@@ -29,8 +38,8 @@ void all_lover(char* word);
 void is_verb(char verb[SIZE]);
 void all_lower(char* word);
 
-//Генерує кілька випадкових слів тіпа денаціифакція, біолабораторії та інші скрєпні речі
-void Pitun_speak();
+//Генерує n випадкових слів тіпа денаціифакція, біолабораторії та інші скрєпні речі
+void pitun_speak(int nwords);
 
 void scene::set(int ls, char n[SIZE], char t[SIZE*5]){
     landscape = ls;
@@ -210,4 +219,84 @@ void is_verb(char verb[SIZE])
         is_verb(verb);
     }
     f.close();
+}
+
+
+
+
+/*-------------------------------------------------------------------------------------
+ це і нижче пов'язане з функцією мови пітуна, з докладними коментами, як це працює
+-------------------------------------------------------------------------------------
+
+typedef deque<string> умовно контейнер для стрінгу в якому лежить префікс
+vector<string> теж контейнер для стрінгів(лежить суфікс) 
+ принципово робити різні, бо в них трошки різні фічі
+ map - щось типу аналог словників на пітоні, пара: ключ, значення, де ключ це наш префікс, значення - суфікс
+ по певному префіксу програмка вибирає суфікс, суть рандомізації, що до однакових префіксів шляхом 
+ повторення слів в реченнях можуть прив'язатись різні суфікси і воно, будуючи текст, може перескакувати, циклитись, 
+ але оскільки наші рандомні префікси/суфікси це зв'язки слів, речення виходять адекватні */
+typedef deque<string> Prefix; 
+
+
+map<Prefix, vector<string>> statetab; 
+
+enum{
+	NPREF = 2,     //кількість елементів у контейнері префіксу  || 1 - тотал рандом,
+                   //3 - треба велику базу щоб тупо не копіювало файлик
+	NHASH = 1000,  //4093, size of state hash table array ((чесно без поняття, у шпури таке було я й написав)) 
+	MAXGEN = 100 //ліміт генерованих слів
+};
+
+char NONWORD[] = "/n"; /*заглушка для контейнерів*/
+
+// add:  додаємо слово в список суфіксів, і завантажуємо нове слово в префікс
+void add(Prefix& prefix, const string& s){
+	if (prefix.size() == NPREF){
+		statetab[prefix].push_back(s);
+		prefix.pop_front();
+	}
+	prefix.push_back(s);
+}
+
+// build: відкриваємо файл, з нього зчитуємо по слову і додаємо в список, вибудовуючи тим самим хеш-таблицю
+void build(Prefix& prefix/*, istream& in*/) {
+	string buf;
+	ifstream textfile;
+	textfile.open("kubli.txt");
+	if(!textfile.is_open()){
+		cout << "Error: cannot open pitun.txt file" << endl; 
+		return;
+	}	
+	while(textfile >> buf)
+		add(prefix, buf);
+		
+}
+
+//generate: генеруємо репліки, по одному слову через пробіл
+void generate(int nwords) {
+	Prefix prefix;
+	int i;
+	for (i=0; i<NPREF; i++) //скидуємо префікс і робимо його з заглушок, щоб початок працював коректно
+		add(prefix, NONWORD);
+	for (i=0; i<nwords; i++){
+		vector<string>& suf = statetab[prefix];
+		const string& w = suf[rand() % suf.size()];
+		if (w == NONWORD)
+			break;
+		cout << w << " ";
+		prefix.pop_front(); // просуваємось на один префікс вперед
+		prefix.push_back(w);
+	}
+}
+
+void pitun_speak(int nwords) {
+	if (nwords > MAXGEN) {nwords = MAXGEN;} //щоб не вилізати за рамки сцени
+	srand(time(0)); //сід для рандому
+	Prefix prefix; // поточний префікс 
+	for (int i=0; i<NPREF; i++) //ініціалізуємо його заглушками
+		add(prefix, NONWORD);
+	build(prefix/*, cin*/); // зчитуємо файл, формуємо таблицю
+	add(prefix, NONWORD);  // в результаті білд у нас префікс стоїть в кінці тексту, скидуємо його
+	generate(nwords);     // друк реплік.
+
 }
