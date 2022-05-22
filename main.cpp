10545397aaa3773f5caa4d071b788790ee208b1e
Vitalii <solonychnui.vitaliy@lll.kpi.ua>
2022-05-22 20:38:38 +0200

    фіналочка

--- main.cpp
+++ main.cpp
@@ -3,16 +3,17 @@
 int main() {
     scene script[N]; // масив сцен
     input info; // об'єкт структури для збереження даних, що вводить користувач
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    
-    intro(); // функція, що ознайомлює користувача з ідеєю гри
-    
-    do { // зациклюємо програму
-        f_input(&info); // функція вводу даних користувачем
-        cut_paste(info, &script[0]);
 
-        storytale(0, 6, script);
-        if (choice_f())
+    do { // зациклюємо програму
+        counter=0;//Занулюємо кількість кроків
+        intro(); // функція, що ознайомлює користувача з ідеєю гри
+        //В змінну t1 записуємо час в момент запуску програми
+        high_resolution_clock::time_point t1 = high_resolution_clock::now();
+        f_input(&info); // функція вводу даних користувачем
+        cut_paste(info, &script[0]);//Записуємо пермоналізований сценарій
+
+        storytale(0, 6, script);//Виводить сцени в проміжку [0;6)
+        if (choice_f())//Перший вибір користувача
         { // В залежності від вибору починати з різних сцен
             storytale(6, 10, script);
         }
@@ -21,7 +22,7 @@ int main() {
         if (stoi(info.numeric) < 1500)
         { // Сюжетна розвилка
             storytale(20, 32, script);
-            if (choice_s()){
+            if (choice_s()){ //Другий вибір користувача
                 storytale(32, 48, script);
             } else {
                 storytale(48, 68, script);
@@ -29,10 +30,10 @@ int main() {
         } else {
             storytale(68, 85, script);
         }
+        //В змінну t2 записуємо час в момент кінця показу історії
         high_resolution_clock::time_point t2 = high_resolution_clock::now();
-        
         outro(); // функція з "титрами"
-        technical_report(t1, t2);
-    } while (loop() == true);
+        technical_report(t1, t2);//Виводить технічні дані
+    } while (loop());//Перевірка, чи повторно запустити програму
     return 0;
 }
